b8c7bb11e724045f4fe56d5c50eaae41e0c8dda0
AhmedDuraid <ahmeduraid123@gmail.com>
2020-09-28 19:06:29 +0400

    Added article connection with the database

---
+++ ADHDataManager.Library/DataAccess/ArticleData.cs
@@ -0,0 +1,51 @@
+﻿using ADHDataManager.Library.Internal.DataAccess;
+using ADHDataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace ADHDataManager.Library.DataAccess
+{
+    public class ArticleData
+    {
+        private readonly string ConnectionName = "AHDConnection";
+
+        public List<ArticleModel> GetArticles()
+        {
+            SqlDataAccess sqlDataAccess = new SqlDataAccess();
+
+            var Parameters = new { @WithUserInfo = 0 };
+
+            var output = sqlDataAccess.LoadData<ArticleModel, dynamic>("dbo.spArticles_GetAllArticles", Parameters,
+                 ConnectionName);
+
+            return output;
+        }
+
+        public List<ArticleModel> GetArticleByID(int id)
+        {
+            SqlDataAccess sqlDataAccess = new SqlDataAccess();
+
+            var Parameters = new { @WithUserInfo = 0, @ID = id };
+
+            var output = sqlDataAccess.LoadData<ArticleModel, dynamic>("dbo.spArticles_GetArticleByID", Parameters,
+                 ConnectionName);
+
+            return output;
+
+        }
+
+        public void AddArticle(ArticleModel article)
+        {
+            SqlDataAccess sqlDataAccess = new SqlDataAccess();
+
+            var Parameters = new
+            {
+                @ArticalTitel = article.article_titel,
+                @ArticleBody = article.article_body,
+                @UserId = article.user_id
+            };
+
+            sqlDataAccess.SaveData<dynamic>("dbo.sp_Articles_CreateArticle", Parameters, ConnectionName);
+
+        }
+    }
+}
---
+++ ADHDataManager.Library/Models/ArticleModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ADHDataManager.Library.Models
+{
+    public class ArticleModel
+    {
+        public int article_id { get; set; }
+        public string article_titel { get; set; }
+        public string article_body { get; set; }
+        public DateTime created_date { get; set; }
+        public DateTime last_update { get; set; }
+        public bool show { get; set; }
+        public int user_id { get; set; }
+    }
+}
