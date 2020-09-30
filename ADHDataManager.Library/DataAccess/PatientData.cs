0616c36db05f1c0fbe9646cd0e17002f5ca4b5e0
AhmedDuraid <ahmeduraid123@gmail.com>
2020-09-30 14:44:25 +0400

    added basic patient data connections, GET, GET by ID, ADD new

--- ADHDataManager.Library/DataAccess/PatientData.cs
+++ ADHDataManager.Library/DataAccess/PatientData.cs
@@ -1,6 +1,57 @@
-﻿namespace ADHDataManager.Library.DataAccess
+﻿using ADHDataManager.Library.Internal.DataAccess;
+using ADHDataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace ADHDataManager.Library.DataAccess
 {
     public class PatientData
     {
+        private readonly string ConnectionName = "AHDConnection";
+        public List<PatientModel> GetPatients()
+        {
+            SqlDataAccess sqlDataAccess = new SqlDataAccess();
+
+            var output = sqlDataAccess.LoadData<PatientModel, dynamic>("dbo.spPatients_GetPatients",
+                new { }, ConnectionName);
+            return output;
+
+
+        }
+        public List<PatientModel> GetPatientByID(int id)
+        {
+            SqlDataAccess sqlDataAccess = new SqlDataAccess();
+            var Parameters = new { @PatientId = id };
+
+            var output = sqlDataAccess.LoadData<PatientModel, dynamic>("dbo.spPatients_GetPatientByID",
+                Parameters, ConnectionName);
+            return output;
+
+
+        }
+        public void AddPatient(PatientModel patient)
+        {
+            SqlDataAccess sqlDataAccess = new SqlDataAccess();
+            var Parameters = new
+            {
+                @FirstName = patient.first_name,
+                @LastName = patient.last_name,
+                @DirthDate = patient.birth_date,
+                @Gender = patient.gender,
+                @Email = patient.email,
+                @Nationality = patient.nationality,
+                @Weight = patient.weight,
+                @Height = patient.height,
+                @Bmi = patient.bmi,
+                @UserName = patient.user_name,
+                @Password = patient.password,
+                @MiddleName = patient.middle_name
+            };
+
+            sqlDataAccess.SaveData<dynamic>("dbo.spPatients_AddPatient",
+                Parameters, ConnectionName);
+
+
+
+        }
     }
 }
--- ADHDataManager.Library/Models/PatientModel.cs
+++ ADHDataManager.Library/Models/PatientModel.cs
@@ -1,6 +1,22 @@
-﻿namespace ADHDataManager.Library.Models
+﻿using System;
+
+namespace ADHDataManager.Library.Models
 {
     public class PatientModel
     {
+        public int patient_id { get; set; }
+        public string first_name { get; set; }
+        public string middle_name { get; set; }
+        public string last_name { get; set; }
+        public DateTime birth_date { get; set; }
+        public string gender { get; set; }
+        public string email { get; set; }
+        public string nationality { get; set; }
+        public double weight { get; set; }
+        public double height { get; set; }
+        public double bmi { get; set; }
+        public string user_name { get; set; }
+        public string password { get; set; }
+
     }
 }
