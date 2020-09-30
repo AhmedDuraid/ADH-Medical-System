a5d4750a21d42127c06f3c2aa3b3566fe17857ca
AhmedDuraid <ahmeduraid123@gmail.com>
2020-09-30 14:04:56 +0400

    Added basic PatientProgress connection functions, GET, GET by ID, GET by Patient ID, ADD new progress

--- ADHDataManager.Library/DataAccess/PatientProgressData.cs
+++ ADHDataManager.Library/DataAccess/PatientProgressData.cs
@@ -1,6 +1,62 @@
-﻿namespace ADHDataManager.Library.DataAccess
+﻿using ADHDataManager.Library.Internal.DataAccess;
+using ADHDataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace ADHDataManager.Library.DataAccess
 {
     public class PatientProgressData
     {
+
+        private readonly string ConnectionName = "AHDConnection";
+
+        public List<PatientProgressModel> GetPatientProgresses()
+        {
+            SqlDataAccess sqlDataAccess = new SqlDataAccess();
+
+            var output = sqlDataAccess.LoadData<PatientProgressModel, dynamic>("dbo.spPatientProgress_GetProgresses",
+                new { }, ConnectionName);
+            return output;
+
+
+        }
+        public List<PatientProgressModel> GetPatientProgressesById(int progressID)
+        {
+            SqlDataAccess sqlDataAccess = new SqlDataAccess();
+            var Parameters = new { @ID = progressID };
+
+            var output = sqlDataAccess.LoadData<PatientProgressModel, dynamic>("dbo.spPatientProgress_GetProgressByID",
+                Parameters, ConnectionName);
+            return output;
+
+
+        }
+        public List<PatientProgressModel> GetPatientProgressesByPatientID(int patientID)
+        {
+            SqlDataAccess sqlDataAccess = new SqlDataAccess();
+            var Parameters = new { @PatientID = patientID };
+
+            var output = sqlDataAccess.LoadData<PatientProgressModel, dynamic>("dbo.spPatientProgress_GetProgressByPatientID",
+                Parameters, ConnectionName);
+            return output;
+
+
+        }
+        public void AddProgress(PatientProgressModel progress)
+        {
+            SqlDataAccess sqlDataAccess = new SqlDataAccess();
+            var Parameters = new
+            {
+                @Weight = progress.weight,
+                @BMI = progress.bmi,
+                @UserID = progress.user_id,
+                @PatientID = progress.patient_Id,
+                @AddedBY = progress.added_by
+            };
+
+            sqlDataAccess.SaveData<dynamic>("dbo.spPatientProgress_ADDProgress",
+                    Parameters, ConnectionName);
+
+
+        }
     }
 }
--- ADHDataManager.Library/Models/PatientProgressModel.cs
+++ ADHDataManager.Library/Models/PatientProgressModel.cs
@@ -1,6 +1,21 @@
-﻿namespace ADHDataManager.Library.Models
+﻿using System;
+
+namespace ADHDataManager.Library.Models
 {
     public class PatientProgressModel
     {
+        // table 
+        public int progress_id { get; set; }
+        public DateTime date { get; set; }
+        public float weight { get; set; }
+        public float bmi { get; set; }
+        public string added_by { get; set; }
+
+        // realation tables 
+        public int patient_Id { get; set; }
+        public int user_id { get; set; }
+        public string patient_first_name { get; set; }
+
+
     }
 }
