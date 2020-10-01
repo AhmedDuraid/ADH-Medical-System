41b73b52ed66c87697baca4d5958dbe05e34e5f6
AhmedDuraid <ahmeduraid123@gmail.com>
2020-10-01 14:46:35 +0400

    added Plan controller. API functions has been tested and works well
    added Patient controller. API functions has been tested and works well

---
+++ ADHMedicalSystemAPI/Controllers/PatientController.cs
@@ -0,0 +1,43 @@
+﻿using ADHDataManager.Library.DataAccess;
+using ADHDataManager.Library.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace ADHMedicalSystemAPI.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Patient")]
+    public class PatientController : ApiController
+    {
+        // GET: api/Patient
+        public List<PatientModel> Get()
+        {
+            PatientData patientData = new PatientData();
+
+            var Patients = patientData.GetPatients();
+
+            return Patients;
+        }
+
+        // GET: api/Patient/{id}
+        public List<PatientModel> Get(int id)
+        {
+            PatientData patientData = new PatientData();
+
+            var Patient = patientData.GetPatientByID(id);
+
+            return Patient;
+
+        }
+
+        // POST: api/Patient
+        public void Post([FromBody] PatientModel patient)
+        {
+            PatientData patientData = new PatientData();
+
+            patientData.AddPatient(patient);
+        }
+
+
+    }
+}
