510b433fe69b77e0688aa798f6bfa4ba33493119
AhmedDuraid <ahmeduraid123@gmail.com>
2020-10-20 14:55:32 +0400

    add auth to feedback controller, add more endpoints

--- ADHApi/Controllers/StaffAndPatients/FeedbackController.cs
+++ ADHApi/Controllers/StaffAndPatients/FeedbackController.cs
@@ -1,12 +1,14 @@
 ﻿using ADHDataManager.Library.DataAccess;
 using ADHDataManager.Library.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace ADHApi.Controllers.StaffAndPatients
 {
-    [Route("api/staffAndPatients/[controller]/[action]")]
+    [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class FeedbackController : ControllerBase
     {
         private readonly IFeedbackData _feedbackData;
@@ -16,51 +18,86 @@ namespace ADHApi.Controllers.StaffAndPatients
             _feedbackData = feedbackData;
         }
 
-        // GET: api/staffAndPatients/Feedback/GetFeedbacks
-        [HttpGet]
+        // GET: api/Feedback/
+        [HttpGet("Admin")]
+        [Authorize(Roles = "Admin")]
         public IActionResult GetFeedbacks()
         {
-
             var feedbacks = _feedbackData.GetFeedbacks();
 
-            return Ok(feedbacks);
+            if (feedbacks.Count > 0)
+            {
+                return Ok(feedbacks);
+            }
 
-
+            return NotFound();
         }
 
-        // GET: api/staffAndPatients/Feedback/GetFeedbacksBotReaded
-        [HttpGet]
-        public List<FeedbackModel> GetFeedbacksBotReaded()
+        // GET: api/Feedback
+        [HttpGet("NotReaded")]
+        [Authorize(Roles = "Admin, Manager")]
+        public IActionResult GetFeedbacksNotReaded()
         {
             var Feedback = _feedbackData.GetFeedbackByNotReaded();
 
-            return Feedback;
+            if (Feedback.Count > 0)
+            {
+                return Ok(Feedback);
+
+            }
+
+            return NotFound();
         }
 
-        // GET: api/staffAndPatients/Feedback/GetFeedbackReaderID/id
-        [HttpGet("{readerId}")]
-        public List<FeedbackModel> GetFeedbackReaderID(string readerId)
+        // GET: api/Feedback
+        [HttpGet("GetById")]
+        [Authorize(Roles = "Manager")]
+        public IActionResult GetFeedbackReaderID()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var Feedback = _feedbackData.GetFeedbackByReaderId(userId);
+
+            if (Feedback.Count > 0)
+            {
+                return Ok(Feedback);
+            }
+
+            return NotFound();
+        }
+
+        // GET: api/Feedback/readerId
+        [HttpGet("Admin/{readerId}")]
+        public IActionResult GetFeedbackReaderID(string readerId)
         {
             var Feedback = _feedbackData.GetFeedbackByReaderId(readerId);
 
-            return Feedback;
+            if (Feedback.Count > 0)
+            {
+                return Ok(Feedback);
+
+            }
+
+            return NotFound();
         }
 
-        // POST: api/staffAndPatients/Feedback/AddFeedback/id
+        // POST: api/Feedback
         [HttpPost]
+        [AllowAnonymous]
         public void AddFeedback([FromBody] FeedbackModel feedback)
         {
             _feedbackData.AddNewFeedback(feedback);
-
         }
 
-        // POST: api/staffAndPatients/Feedback/UpdateFeedback/id
+        // POST: api/Feedback
         [HttpPut]
-        public IActionResult UpdateFeedback([FromBody] string readerId, string feedbackId)
+        [Authorize(Roles = "Manager, Admin")]
+        public IActionResult UpdateFeedback([FromQuery] string feedbackId)
         {
+            var readerId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
             _feedbackData.UpdateFeedbackToReaded(readerId, feedbackId);
 
-            return Ok();
+            return Ok("Updated");
         }
     }
 }
