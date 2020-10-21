1a47140646b1ae6ece6479136e18bb95bb3ed994
AhmedDuraid <ahmeduraid123@gmail.com>
2020-10-21 19:35:41 +0400

    add Api model

---
+++ ADHApi/Models/ApiPatientProgressModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ADHApi.Models
+{
+    public class ApiPatientProgressModel
+    {
+        public float Weight { get; set; }
+        public float BMI { get; set; }
+        public string PatientId { get; set; }
+        public string AddedBy { get; set; }
+    }
+}
