8c675d2cc3ca70fd07db5db02eb16d413f362b96
AhmedDuraid <ahmeduraid123@gmail.com>
2020-10-21 12:11:51 +0400

    add swagger config to use token

--- ADHApi/Startup.cs
+++ ADHApi/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace ADHApi
@@ -85,6 +86,7 @@ namespace ADHApi
 
             });
 
+            // swagger steup
             services.AddSwaggerGen(setup =>
             {
                 setup.SwaggerDoc(
@@ -95,6 +97,37 @@ namespace ADHApi
                         Version = "v1"
                     }
                     );
+
+                setup.AddSecurityDefinition("Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer"
+                    });
+
+                setup.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                                {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                                },
+                                Scheme = "oauth2",
+                                Name = "Bearer",
+                                In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                     }
+                });
             });
         }
 
