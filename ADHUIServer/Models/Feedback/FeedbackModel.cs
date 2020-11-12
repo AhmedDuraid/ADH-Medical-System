ec1bd3ec16a834a299f877502ea32fef03593718
AhmedDuraid <ahmeduraid123@gmail.com>
2020-11-12 22:00:02 +0400

    add admin feedback page with feedbacks table "no style"
    add model for public and staff feedbacks

--- ADHUIServer/Models/Feedback/FeedbackModel.cs
+++ ADHUIServer/Models/Feedback/FeedbackModel.cs
@@ -1,25 +1,19 @@
-﻿using FluentValidation;
+﻿using System;
 
 namespace ADHUIServer.Models.Feedback
 {
     public class FeedbackModel
     {
+        public string Id { get; set; }
         public string Titel { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
         public string FeedbackBody { get; set; }
-    }
-
-    public class FeedbackValidation : AbstractValidator<FeedbackModel>
-    {
-        public FeedbackValidation()
-        {
-            RuleFor(x => x.Titel).Length(6, 50).NotEmpty();
-            RuleFor(x => x.Name).Length(6, 50).NotEmpty();
-            RuleFor(x => x.Email).Length(6, 50).NotEmpty().EmailAddress();
-            RuleFor(x => x.Phone).Length(8, 13).NotEmpty();
-            RuleFor(x => x.FeedbackBody).Length(20, 300).NotEmpty();
-        }
+        public DateTime Date { get; set; }
+        public bool HasBeenRead { get; set; }
+        public string ReaderId { get; set; }
+        public string ReaderFirstName { get; set; }
+        public string ReaderLastName { get; set; }
     }
 }
