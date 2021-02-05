e4f4f5517c3902533c9a21ff79bbcf9e5ea1fbfb
AhmedDuraid <ahmeduraid123@gmail.com>
2021-02-05 13:40:28 +0400

    Initial creation for DB project

--- ADHApi/Controllers/SampleController.cs
+++ ADHApi/Controllers/SampleController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +8,8 @@ namespace ADHApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
+
     public class SampleController : ControllerBase
     {
         // GET: api/<SampleController>
